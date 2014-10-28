1abed62d73e442a11cf6671ac61bede27eec5d01
usuario <v.rojas1990@gmial.com>
2014-10-28 21:38:27 +0100

    Situacion Inicial

---
+++ HolaMundo1/src/controlador/Principal.java
@@ -0,0 +1,16 @@
+package controlador;
+
+import modelo.Saluda;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Saluda saludo=new Saluda();
+		saludo.diHola("Victor");
+	}
+
+}
---
+++ HolaMundo1/src/modelo/Saluda.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Saluda {
+
+	public Saluda() {
+		
+	}
+	public void diHola(String nombre){
+		System.out.println("Hola:  "+ nombre + " que tal...");
+	}
+}
